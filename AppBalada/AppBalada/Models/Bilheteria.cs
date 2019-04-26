6e1017c4b412b1c9830956ef641cb484fba1ca6d
albertr-fernandes <alberfernandes2010@gmail.com>
2019-04-25 23:48:12 -0300

    Funcionando

---
+++ AppBalada/AppBalada/Models/Bilheteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppBalada.Models
+{
+    public class Bilheteria
+    {
+        public int BilheteriaId { get; set; }
+        public string Nome { get; set; }
+        public virtual ICollection<Ingresso> Ingressos { get; set; }
+    }
+}
